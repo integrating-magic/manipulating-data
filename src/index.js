21584e9ab2f6e4b3d775d9603d8d13603f2b4957
Jeremy Brown <jlbmagic@gmail.com>
2022-02-28 07:59:24 -0500

    added data, removed extra

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-//Here we're importing items we'll need. You can add other imports here.
-
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
-
-//
+import { data } from "./data";
