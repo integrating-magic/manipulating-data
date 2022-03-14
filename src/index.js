4faee043d7c1e8627d7d36891b6412833e1608cc
Jeremy Brown <jlbmagic@gmail.com>
2022-03-14 07:22:34 -0400

    set up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import { companiesData } from "./companies-data";
+import { salesData } from "./sales-data";
+
+const before = document.getElementById("before");
+before.innerHTML = JSON.stringify(companiesData, null, 2);
+
+//functions are here
