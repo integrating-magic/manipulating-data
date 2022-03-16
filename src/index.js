78bd540a7fa54549be4934b03b84f49a2cf8e2d8
Jeremy Brown <jlbmagic@gmail.com>
2022-03-16 15:02:28 -0400

    training we did together

--- src/index.js
+++ src/index.js
@@ -4,4 +4,25 @@ import { salesData } from "./sales-data";
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
+function constructNewObject(e) {
+  const obj = {
+    city: e.fieldData.City,
+    state: e.fieldData.State,
+    cityState: `City / State: ${e.fieldData.City}, ${e.fieldData.State}`,
+  };
+  return obj;
+}
+
+const filterData = function (e) {
+  return e.fieldData.State === "TX";
+};
+
+const manipulatedData = companiesData
+  .filter(filterData) //only show texas records
+  .map(constructNewObject); //constrcut the new object with those TX records.
+
+// const manipulatedData = companiesData.filter(filterData);
+const after = document.getElementById("after");
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+
 //functions are here
