0d0248ce2089f48ee999d7eb81008b1e5f79de1f
Jeremy Brown <jlbmagic@gmail.com>
2022-04-19 08:53:34 -0400

    updated for next binge group

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import { companiesData } from "./companies-data";
-import { salesData } from "./sales-data";
+
+const extraRecord = {
+  title: "JS in FM Training",
+  type: "Binge Mode",
+  cohort: "2",
+  startDate: "2022-04-12",
+};
 
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
+//TODO: Extract an element from the array.
+//TODO: Add elements to beginning and end of array.
+//TODO: Remove Elements from end of array.
+//TODO: Create a new array from old data.
+//TODO: Filter for a certain state.
+//TODO: Find the first element with a certain name.
+//TODO: Create a new array with just elements of a certain state.
+//TODO: Create buttons for each record.
 //functions are here
-const manipulateData = companiesData[0].fieldData;
-const keys = Object.keys(manipulateData);
+const manipulatedData = [];
 
-const after = document.getElementById("after");
-after.innerHTML = JSON.stringify(keys, null, 2);
+after.innerHTML = JSON.stringify(manipulatedData, null, 2);
