2738838595158bcd9f6485561eb70ba59226a82c
Jeremy Brown <jlbmagic@gmail.com>
2022-08-16 08:43:01 -0400

    updated changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import { companiesData } from "./companies-data";
 
-
-
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-//TODO: object functions.
-//TODO: Extract an element from the array.
-
-//TODO: Add elements and remove to beginning and end of array.
-
-//TODO: Create a new array from old data.
-//TODO: Filter for a certain state.
-//TODO: Find the first element with a certain name.
-//TODO: Create a new array with just elements of a certain state.
-//TODO: Create buttons for each record.
-//functions are here
 const manipulatedData = [];
-
-//TODO: write manipulated data to the 'after' div.
+//TODO: add the manipulated data array to the 'after' div.
