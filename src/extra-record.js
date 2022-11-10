ff2bdbd9d7eb0d8b60bc1f0a01e26785a12f531d
Jeremy Brown <jlbmagic@gmail.com>
2022-11-10 06:56:37 -0500

    update the extra record

--- src/extra-record.js
+++ src/extra-record.js
@@ -1,6 +1,14 @@
 export const extraRecord = {
-  title: "August JS in FM Training",
-  type: "Relax Mode",
-  cohort: "7",
-  startDate: "2022-08-04",
+  fieldData: {
+    City: "Winter Garden",
+    CompanyName: "Integrating Magic, LLC",
+    Id: "62406271-0C52-4F53-4I94-6879B253C555",
+    ModifiedTimeStamp: "12/22/2021 20:02:35",
+    State: "FL",
+    StreetAddress: "659 Stevelynn Cir",
+    Zip: "34787",
+  },
+  modId: "0",
+  portalData: {},
+  recordId: "1",
 };
